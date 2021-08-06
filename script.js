672e5822d527fb74eeb903956b738aebfae2b57d
g3andre <andre.mmp@gmail.com>
2021-08-06 13:00:15 -0300

    Adicionado filters e modal

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ const app = new Vue({
 	el: '#app',
 	data: {
 		produtos: [],
+		produto: false,
 	},
 	created() {
-    this.retrieveProducts();
-  },
+		this.retrieveProducts();
+	},
+	filters: {
+		formatCurrency(value) {
+			return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+		},
+	},
 	methods: {
 		retrieveProducts() {
 			fetch('./api/produtos.json')
 				.then((response) => response.json())
 				.then((data) => (this.produtos = data));
 		},
+		retrieveProduct(productId) {
+			fetch(`./api/produtos/${productId}/dados.json`)
+				.then((response) => response.json())
+				.then((data) => (this.produto = data));
+		},
 	},
-  computed: {
-    formatCurrency(value) {
-      return `R$ ${parseFloat(value).toFixed(2)}`
-    }
-  }
+	computed: {
+		formatCurrency(value) {
+			return `R$ ${parseFloat(value).toFixed(2)}`;
+		},
+	},
 });
