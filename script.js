5e2bc87db07c994dc87690353f27d3911d8d65d2
g3andre <andre.mmp@gmail.com>
2021-08-09 13:15:15 -0300

    Implementado carrinho de compras

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ const app = new Vue({
 	data: {
 		produtos: [],
 		produto: false,
+		carrinho: [],
+		alertaAtivo: false,
+		mensagemAlerta: '',
+		carrinhoAtivo: false,
 	},
 	created() {
 		this.retrieveProducts();
+		this.checaLocalStorage();
+		this.acessaUrl();
 	},
 	filters: {
 		formatCurrency(value) {
@@ -26,10 +32,68 @@ const app = new Vue({
 		fecharModal({ target, currentTarget }) {
 			if (target == currentTarget) this.produto = false;
 		},
+		adicionarItem() {
+			this.produto.estoque--;
+			let { id, nome, preco } = this.produto;
+			this.carrinho.push({ id, nome, preco });
+			this.alerta(`${nome} adicionado ao carrinho.`);
+		},
+		checaLocalStorage() {
+			if (window.localStorage.carrinho) this.carrinho = JSON.parse(window.localStorage.carrinho);
+		},
+		alerta(mensagem) {
+			this.mensagemAlerta = mensagem;
+			this.alertaAtivo = true;
+			setTimeout(() => {
+				this.alertaAtivo = false;
+			}, 2000);
+		},
+		acessaUrl() {
+			let url = location.hash;
+			if (url) this.retrieveProduct(url.replace('#', ''));
+		},
+		clickForaCarrinho({ target, currentTarget }) {
+			if (target == currentTarget) this.carrinhoAtivo = false;
+		},
+		removerItem(index) {
+			this.carrinho.splice(index, 1);
+		},
 	},
 	computed: {
 		formatCurrency(value) {
 			return `R$ ${parseFloat(value).toFixed(2)}`;
+		},
+		carrinhoTotal() {
+			return this.carrinho.reduce((acc, current) => {
+				return acc + current.preco;
+			}, 0);
+		},
+		carrinhoEditado() {
+			const teste = this.carrinho.reduce((acc, item) => {
+				const indexElement = acc.find((element, index) => {
+					if (element.id == item.id) {						
+						acc[index].qtd++;
+						return true;
+					}
+				});
+				
+				if (!indexElement) {
+					acc.push({ ...item, qtd: 1 });
+				}
+				return acc;
+			}, []);
+			return teste;
+		},
+	},
+	watch: {
+		carrinho() {
+			window.localStorage.carrinho = JSON.stringify(this.carrinho);
+			console.log(this.carrinhoEditado);
+		},
+		produto() {
+			document.title = this.produto.nome || 'Techno';
+			let hash = this.produto.id || '';
+			history.pushState(null, null, `#${hash}`);
 		},
 	},
 });
